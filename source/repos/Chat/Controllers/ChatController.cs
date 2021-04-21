f40075d5d46fa556cc43609a4ffa19ce61482644
[antonycochet] <[cochetantony@gmail.com]>
2021-04-21 12:41:31 +0200

    Tp Chat

---
+++ source/repos/Chat/Controllers/ChatController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Chat.Models;
+
+namespace Chat.Controllers
+{
+    public class ChatController : Controller
+    {
+
+        public List<ChatModels> Chats = ChatModels.GetMeuteDeChats();
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View(Chats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            var chat = Chats.FirstOrDefault(c => c.Id == id);
+            if (chat == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(chat);
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var chat = Chats.FirstOrDefault(c => c.Id == id);
+            if (chat == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(chat);
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                var chat = Chats.FirstOrDefault(c => c.Id == id);
+                if (chat != null)
+                {
+                    Chats.Remove(chat);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ source/repos/Chat/Models/ChatModels.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Chat.Models
+{
+    public class ChatModels
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int Age { get; set; }
+        public string Couleur { get; set; }
+
+        public static List<ChatModels> GetMeuteDeChats()
+        {
+            var i = 1;
+            return new List<ChatModels>
+            {
+                new ChatModels{Id=i++,Nom = "Felix",Age = 3,Couleur = "Roux"},
+                new ChatModels{Id=i++,Nom = "Minette",Age = 1,Couleur = "Noire"},
+                new ChatModels{Id=i++,Nom = "Miss",Age = 10,Couleur = "Blanche"},
+                new ChatModels{Id=i++,Nom = "Garfield",Age = 6,Couleur = "Gris"},
+                new ChatModels{Id=i++,Nom = "Chatran",Age = 4,Couleur = "Fauve"},
+                new ChatModels{Id=i++,Nom = "Minou",Age = 2,Couleur = "Blanc"},
+                new ChatModels{Id=i,Nom = "Bichette",Age = 12,Couleur = "Rousse"}
+            };
+        }
+    }
+}
