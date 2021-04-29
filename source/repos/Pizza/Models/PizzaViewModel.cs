88d9ed79a63423be31f3e3bcacc927f16e1e8450
[antonycochet] <[cochetantony@gmail.com]>
2021-04-29 22:26:30 +0200

    PizzaTp

---
+++ source/repos/Pizza/Models/PizzaViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using System.Threading.Tasks;
+using Pizza.Utils;
+using Pizza.Models;
+
+namespace Pizza.Models
+{
+    public class PizzaViewModel
+    {
+        public PizzaModel Pizza { get; set; }
+        public List<SelectListItem> Ingredients { get; set; } = new List<SelectListItem>();
+        public List<SelectListItem> Pates { get; set; } = new List<SelectListItem>();
+
+        [Required]
+        public int? IdPate { get; set; }
+        public List<int> IdsIngredients { get; set; } = new List<int>();
+
+
+    }
+}
